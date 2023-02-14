ddd9c073172b6709544119cc09e5bdb9d1c7a709
leobaek <120348541+leobaek@users.noreply.github.com>
2023-02-14 14:52:09 +0900

    Initial commit

---
+++ app/src/main/java/com/leo104/postingapp/api/PostingApi.java
@@ -0,0 +1,23 @@
+package com.leo104.postingapp.api;
+
+import com.leo104.postingapp.model.Res;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface PostingApi {
+
+    // 포스팅 생성 API
+    @Multipart
+    @POST("/posting")
+    Call<Res> addPosting(@Header("Authorization") String token,
+                         @Part MultipartBody.Part photo,
+                         @Part("content")RequestBody content);
+
+}
+
---
+++ app/src/main/java/com/leo104/postingapp/config/Config.java
@@ -0,0 +1,12 @@
+package com.leo104.postingapp.config;
+
+public class Config {
+
+    public static final String DOMAIN = "https://3x987mvf29.execute-api.ap-northeast-2.amazonaws.com";
+    public static final String DOMAIN_TEST = "http://localhost:5000";
+    public static final String PREFERENCE_NAME = "memo_app";
+    public static final String ACCESS_TOKEN = "access_token";
+
+
+
+}
